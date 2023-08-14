61f147393cc7cb3940f4462ee4ff5da8c155db6d
Juliana Haurilava <jrediss.k@gmail.com>
2023-08-14 15:10:35 +0300

    second task done

--- Assets/Scripts/RelayRace.cs
+++ Assets/Scripts/RelayRace.cs
@@ -1,18 +1,21 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RelayRace : MonoBehaviour
 {
-    [SerializeField] float Speed;           // скорость передвижения объекта
+    private const int RUNNERS_AMOUNT = 5;       // количество бегунов
+    private const float MIN_DISTANCE = 1.3f;    // минимальное допустимое расстояние между бегунами
 
+    [SerializeField] float Speed;               // скорость передвижения объектов
 
-    Queue<Transform> runners;                    // массив бегунов
-    private const int RUNNERS_AMOUNT = 5;   // количество бегунов
+    private Queue<Transform> runners;           // очередь бегунов
 
-    private Transform currentRunner;
-    private Transform targetRunner;
+    private Transform previousRunner;           // предыдущий бегун
+    private Transform currentRunner;            // текущий бегун
+    private Transform targetRunner;             // цель текущего бегуна
 
+    private Vector3 previousTarget;             // координаты предыдущей цели
 
     void Start()
     {
@@ -21,23 +24,45 @@ public class RelayRace : MonoBehaviour
 
         currentRunner = runners.Dequeue();
         targetRunner = runners.Peek();
-
-        currentRunner.transform.LookAt(targetRunner.position);
     }
 
+
+    /// <summary>
+    /// Инициализация очереди из бегунов
+    /// </summary>
     private void InitializeRunners()
     {
         for (int i = 0; i < RUNNERS_AMOUNT; i++)
         {
-            runners.Enqueue(transform.Find($"Runner {i + 1}"));
+            Transform runnerToAdd = transform.Find($"Runner {i + 1}");
+            runners.Enqueue(runnerToAdd);
         }
     }
     void Update()
     {
-        currentRunner.position = Vector3.MoveTowards(currentRunner.position, targetRunner.position, Speed * Time.deltaTime);
+        currentRunner.position = Vector3.MoveTowards(currentRunner.position,
+                                 targetRunner.position, Speed * Time.deltaTime);
+        currentRunner.LookAt(targetRunner);
+        LookAtRunner();
 
-        if (currentRunner.position == targetRunner.position)
+        // плавная остановка предыдущего бегуна 
+        if (previousRunner && previousRunner.position != previousTarget)
+        {
+            previousRunner.position = Vector3.Lerp(previousRunner.position,
+                                      previousTarget, 0.1f);
+            float distance = Vector3.Distance(previousTarget, previousRunner.position);
+            double distanceRounded = Math.Round(distance, 2);
+            if (distanceRounded == 0)
+            {
+                previousRunner.position = previousTarget;
+            }
+        }
+        // смена бегунов, если активный бегун подбежал на расстояние MIN_DISTANCE к объекту-цели
+        if (Vector3.Distance(currentRunner.position, targetRunner.position) <= MIN_DISTANCE)
+        {
+            SwitchCoordinates();
             SwitchRunner();
+        }
     }
 
     /// <summary>
@@ -46,8 +71,31 @@ public class RelayRace : MonoBehaviour
     private void SwitchRunner()
     {
         runners.Enqueue(currentRunner);
+        previousRunner = currentRunner;
         currentRunner = runners.Dequeue();
         targetRunner = runners.Peek();
+    }
+
+    /// <summary>
+    /// Меняет координаты для предыдущего бегуна
+    /// </summary>
+    private void SwitchCoordinates()
+    {
+        previousTarget = targetRunner.position;
+    }
+
+    /// <summary>
+    /// Направляет "взгляд" всех не бегущих объектов на активного бегуна
+    /// </summary>
+    private void LookAtRunner()
+    {
+        foreach (Transform runner in runners)
+        {
+            if (runner != currentRunner)
+            {
+                runner.LookAt(currentRunner);
+            }
+        }
     }
 
 }
